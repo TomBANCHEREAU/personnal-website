08328f5e590ba7605243d05a5d518da7ee6707ad
Tom BANCHEREAU <tom.banchereau@viacesi.fr>
2021-01-23 22:04:50 +0100

    chore: setup nuxt-i18n config

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,32 @@ module.exports = {
   srcDir: 'public/',
   quiet: true,
   buildModules: ['@nuxtjs/vuetify'],
-  modules:['nuxt-i18n'],
-  i18n: {},
+  modules: ['nuxt-i18n'],
+  i18n: {
+    strategy: 'prefix',
+    vueI18nLoader: true,
+    defaultLocale: 'fr',
+    // detectBrowserLanguage:{
+    //   alwaysRedirect: false,
+    //   fallbackLocale: '',
+    //   onlyOnRoot: false,
+    //   useCookie: true,
+    //   cookieCrossOrigin: false,
+    //   cookieDomain: null,
+    //   cookieKey: 'i18n_redirected',
+    //   cookieSecure: false
+    // },
+    locales: [
+      {
+        code: 'en',
+        iso: 'en-US',
+        name: 'English'
+      },
+      {
+        code: 'fr',
+        iso: 'fr-FR',
+        name: 'Français'
+      }
+    ]
+  }
 }
